c8774e93beb0603f0cca08c15dd4ab25bab0b8ec
Huzaifa309 <ahmedhuzaifa247@gmail.com>
2025-07-22 15:45:02 +0500

    fmt changes

--- eKYCEngine.cpp
+++ eKYCEngine.cpp
@@ -14,14 +14,14 @@
 eKYCEngine::eKYCEngine() : running_(false) {
     try {
         aeron_ = std::make_unique<aeron_wrapper::Aeron>(AeronDir);
-        Log.info("Connected to Aeron Media Driver...");
+        Log.info_fast("Connected to Aeron Media Driver...");
         subscription_ = aeron_->create_subscription(SubscriptionChannel,  //
                                                     SubscriptionStreamId);
         publication_ = aeron_->create_publication(PublicationChannel,  //
                                                   PublicationStreamId);
         running_ = true;
     } catch (const std::exception& e) {
-        Log.info(std::string("Error: ") + e.what());
+        Log.info_fast(std::string("Error: ") + e.what());
     }
 }
 
@@ -29,7 +29,7 @@ eKYCEngine::~eKYCEngine() { stop(); }
 
 void eKYCEngine::start() {
     if (!running_) return;
-    Log.info("Starting eKYC engine...");
+    Log.info_fast("Starting eKYC engine...");
     // Start background msg processing
     backgroundPoller_ = subscription_->start_background_polling(
         [this](const aeron_wrapper::FragmentData& fragmentData) {
@@ -44,7 +44,7 @@ void eKYCEngine::stop() {
     }
 
     running_ = false;
-    Log.info("eKYC engine stopped.");
+    Log.info_fast("eKYC engine stopped.");
 }
 
 void eKYCEngine::process_message(
